cd0ed9d5126123d17e90d471773fd34a531db57b
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-17 02:31:06 +0300

    first try code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {}
 
     setRange(min, max) {
-
+   this.min = min;
+   this.max = max;
     }
 
     guess() {
-
+    return Math.round((this.max + this.min)/2);
     }
 
     lower() {
-
+    return Math.round((this.max + this.min)/2);
+    
     }
 
     greater() {
-
+    return Math.round((this.max + this.min)/2);
     }
 }
 
